4be93bb5ee4f1206d475c0c1d28ac0b82bf3250b
taksal <harvindersingh89@hotmail.com>
2020-05-20 13:49:26 +1000

    first commit

---
+++ first_project/populate_first_app.py
@@ -0,0 +1,38 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","first_project.settings")
+
+import django
+django.setup()
+## FAKE POP SCRIPT
+
+import random
+from first_app.models import AccessRecord,Webpage,Topic
+from faker import Faker
+
+fakegen = Faker()
+topics =['Search','Social','Marketplace','News','Games']
+def add_topic():
+    t=Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+def populate(N=5):
+    for entry in range(N):
+    #get the topic for entry
+        top = add_topic()
+        #create fkae data for that entry
+
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+    #create new webpage entry
+        webpg = Webpage.objects.get_or_create(Topic=top,url=fake_url,name=fake_name)[0]
+
+    #crearte a fake AccessRecord enrty
+        acc_rec=AccessRecord.objects.get_or_create(name=webpg,date=fake_date)[0]
+
+if __name__ == '__main__':
+    print("population script!")
+    populate(20)
+    print("populationg completee")
